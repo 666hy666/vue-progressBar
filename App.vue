e4d419437755ff60c9546f6e96a243fb60086049
666hy666 <1716497933@qq.com>
2019-08-24 15:01:03 +0800

    progressBar

---
+++ App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div>
+            <h2>线形进度条 -- 百分比外显</h2>
+            <v-progress :percentage="0" :show-text="false"/>
+            <v-progress :percentage="80" color="yellow"/>
+            <v-progress :percentage="100" status="success"/>
+            <v-progress :percentage="30" status="exception"/>
+            <br/>
+
+            <h2>线形进度条 -- 百分比内显</h2>
+            <v-progress :stroke-width="18" :percentage="0" :text-inside='true'/>
+            <v-progress :stroke-width="18" :percentage="80" :text-inside='true'/>
+            <v-progress :stroke-width="18" :percentage="100" status="success" :text-inside='true'/>
+            <v-progress :stroke-width="18" :percentage="60" status="exception" :text-inside='true'/>
+            <br/>
+
+            <h2>环形进度条</h2>
+            <v-progress :percentage="0" type="circle"/>
+            <v-progress :percentage="80" type="circle"/>
+            <v-progress :percentage="100" type="circle" status="success"/>
+            <v-progress :percentage="60" type="circle" status="exception"/>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import VProgress from './Progress';
+export default {
+    components: {
+        VProgress
+    },
+    data(){
+        return{
+            msg: 'hello world'
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
